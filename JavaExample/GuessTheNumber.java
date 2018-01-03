0edc4bffe3166b184c47bbfc14ffad6250c53ab6
A R DANISH <ardnsh001@gmail.com>
2018-01-03 17:14:00 +0530

    Update GuessTheNumber.java

--- JavaExample/GuessTheNumber.java
+++ JavaExample/GuessTheNumber.java
@@ -6,20 +6,26 @@ public class GuessTheNumber {
     Random random = new Random();
     Scanner sc = new Scanner(System.in);
     public void play(){
+	int count=0;
         System.out.println("Welcome to the Number Game Let's Play!");
         System.out.println("Enter the maximum number between which you want to guess !");
         max = sc.nextInt();
         num = Math.abs(random.nextInt()) % max;
         System.out.println("Enter the number you Guess !");
         while(true){
+	    count++;
 	    int insert = sc.nextInt();
             if(insert > num){
-                System.out.println("Your Number is BIG!");
+                System.out.println("Your Number is BIG!");		
             }else if(insert < num){
                 System.out.println("Your Number is Small !");
             }else{
-                System.out.println("You Got My BRO, You Are Awesome !");
-                break;
+		if(count<=3){
+                System.out.println("You Got It On "+count+" Guess"+ " My BRO, You Are Awesome !");
+                }else{
+		System.out.println("You Got It On "+count+" Guess"+ " My BRO !");
+		}
+		break;
             }
         }
     }
